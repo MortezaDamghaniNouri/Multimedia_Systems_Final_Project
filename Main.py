7430b0e067c4227baa1ac1f3f03b48ae53900c19
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-07-11 12:26:03 -0700

    The project is almost done.

--- Main.py
+++ Main.py
@@ -5,22 +5,6 @@ import numpy as np
 # This library is used for reading the image file
 import matplotlib.image as mpimg
 
-import random
-
-
-# This function prints the input matrix
-def matrix_printer(input_matrix):
-    length = len(input_matrix)
-    i = 0
-    while i < length:
-        line = ""
-        j = 0
-        while j < length:
-            line += str(input_matrix[i][j] + "    ")
-            j += 1
-        print(line)
-        i += 1
-
 
 # This function converts RGB input image to grayscale format
 def rgb_to_gray(img):
@@ -39,42 +23,69 @@ def rgb_to_gray(img):
     return grayImage
 
 
+# This function generates dither matrix according to the user input windows size
+def dither_matrix_generator(input_window_size):
+
+    if input_window_size == 2:
+        return [[0, 2], [3, 1]]
+    previous_level_dither_matrix = dither_matrix_generator(input_window_size / 2)
+    output_matrix = []
+    i = 0
+    while i < input_window_size / 2:
+        row = []
+        row_in_previous_level_dither_matrix = i % 4
+        for j in previous_level_dither_matrix[row_in_previous_level_dither_matrix]:
+            row.append(j * 4)
+        for j in previous_level_dither_matrix[row_in_previous_level_dither_matrix]:
+            row.append(j * 4 + 2)
+        output_matrix.append(row)
+        i += 1
+    i = input_window_size / 2
+    while i < input_window_size:
+        row = []
+        row_in_previous_level_dither_matrix = int(i % 4)
+        for j in previous_level_dither_matrix[int(row_in_previous_level_dither_matrix % (input_window_size / 2))]:
+            row.append(j * 4 + 3)
+        for j in previous_level_dither_matrix[int(row_in_previous_level_dither_matrix % (input_window_size / 2))]:
+            row.append(j * 4 + 1)
+        output_matrix.append(row)
+        i += 1
+
+    return output_matrix
+
+
 # This function generates the final dithered image
 def dither_function(input_grayscale_image_location):
-    window_size = int(input("Enter the size of sliding window (for example if it is n * n just enter n): "))
-    image = Image.open(input_grayscale_image_location)
-    new = image.copy()
-    pix = image.load()
-    newpix = new.load()
-    width, height = image.size
+    window_size = int(input("Enter the size of sliding window (for example if it is n * n just enter n and n should be a power of 2): "))
+    dither_matrix = dither_matrix_generator(window_size)
+    L_variable = 256 / (pow(window_size, 2) + 1)
+    grayscale_image = Image.open(input_grayscale_image_location)
+    new_grayscale_image = grayscale_image.copy()
+    grayscale_image_new_pixels = new_grayscale_image.load()
+    width, height = grayscale_image.size
+    i = 0
+    while i < width:
+        j = 0
+        while j < height:
+            grayscale_image_new_pixels[i, j] = (int(grayscale_image_new_pixels[i, j][0] / L_variable), int(grayscale_image_new_pixels[i, j][0] / L_variable), int(grayscale_image_new_pixels[i, j][0] / L_variable))
+            j += 1
+        i += 1
 
-    area = window_size * window_size
-    for i in range(width // window_size):  # loop over pixels
-        for j in range(height // window_size):  # loop over pixels
-            avg = 0
-            area_pix = []
-            for k in range(window_size):
-                for l in range(window_size):
-                    area_pix.append((k, l))  # make a list of coordinates within the tile
-                    try:
-                        avg += pix[window_size * i + k, window_size * j + l][0]
-                        newpix[window_size * i + k, window_size * j + l] = (0, 0, 0)  # set everything to black
-                    except IndexError:
-                        avg += 255 / 2  # just an arbitrary mean value (when were outside of the image)
-                        # this is just a dirty trick for coping with images that have
-                        # sides that are not multiples of window
-            avg = avg / area
-            # val = v is the number of pixels within the tile that will be turned white
-            val = round(avg / 255 * (area + 0.99) - 0.5)  # 0.99 due to rounding errors
-            assert val <= area, 'something went wrong with the val'
-            random.shuffle(area_pix)  # randomize pixel coordinates
-            for m in range(val):
-                rel_coords = area_pix.pop()  # find random pixel within tile and turn it white
-                newpix[window_size * i + rel_coords[0], window_size * j + rel_coords[1]] = (255, 255, 255)
+    i = 0
+    while i < width:
+        j = 0
+        while j < height:
+            row_index = i % window_size
+            column_index = j % window_size
+            if dither_matrix[row_index][column_index] >= grayscale_image_new_pixels[i, j][0]:
+                grayscale_image_new_pixels[i, j] = (0, 0, 0)
+            else:
+                grayscale_image_new_pixels[i, j] = (255, 255, 255)
+            j += 1
+        i += 1
 
-    new.save('Images/dog_dithered_new' + str(window_size) + '.jpg')
-
-
+    new_grayscale_image.save('DITHERED IMAGE' + "(Window Size = " + str(window_size) + ")" + '.jpg')
+    print("Dithered image saved as (DITHERED IMAGE) in the project folder")
 
 
 
@@ -90,6 +101,8 @@ gray_image = rgb_to_gray(image)
 grayscale_output = Image.fromarray(gray_image)
 grayscale_output.save('Grayscale_Input_Image.png')
 dither_function('Grayscale_Input_Image.png')
+
+
 
 
 
